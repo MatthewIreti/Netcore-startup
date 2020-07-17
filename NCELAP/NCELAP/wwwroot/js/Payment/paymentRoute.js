d9dbf875ed8a4a593f557fd4d68483d8705abc9a
dflobt@gmail.com <dflobt@gmail.com>
2020-07-18 00:01:39 +0100

    angular route and validation

---
+++ NCELAP/NCELAP/wwwroot/js/Payment/paymentRoute.js
@@ -0,0 +1,36 @@
+﻿angular.module('ncLasPortalApp').config(['stateHelperProvider', function (stateHelperProvider) {
+    var viewPath = '';
+    stateHelperProvider
+        .state({
+            name: 'site.application',
+            template: '<div ui-view>',
+            url: '/application',
+            abstract: true,
+            children: [
+                {
+                    name: 'list',
+                    url: '/list',
+                    templateUrl: '/App/Application/List.html',
+                    controller: 'ApplicationList'
+                },
+                {
+                    name: 'create',
+                    url: '/create',
+                    controller: 'ApplicationCreate',
+                    templateUrl: '/App/Application/CreateEdit.html'
+                }
+            ]
+        }).state({
+            name: 'site.payment',
+            template: '<div ui-view>',
+            abstract: true,
+            url: '/payment',
+            children: [
+                {
+                    name: 'list',
+                    url: '/list',
+                    templateUrl: '/App/Payment/List.html'
+                }
+            ]
+        });
+}]);
