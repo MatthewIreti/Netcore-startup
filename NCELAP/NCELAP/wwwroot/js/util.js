a133c873547870dd2290f3ad2a5b4afb8f5e67c8
Olufemi Oyedepo <olufemioyedepo@gmail.com>
2020-07-20 21:17:34 +0100

    API endpoint points to SAT, added https as protocol for base url

--- NCELAP/NCELAP/wwwroot/js/util.js
+++ NCELAP/NCELAP/wwwroot/js/util.js
@@ -1,6 +1,6 @@
 ﻿// const baseUrl = 'https://localhost:44310/api/';
 //const baseUrl = 'https://ncelap-demo-api.azurewebsites.net/api/';
-const baseUrl = 'http://ncelaapi.azurewebsites.net/';
+const baseUrl = 'https://ncelaapi.azurewebsites.net/';
 
 
 function extractExtensionFromFileName(fileName) {
