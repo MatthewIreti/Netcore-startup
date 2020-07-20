019768a4758e2f163a052c40a903ed75015790f0
Olufemi Oyedepo <olufemioyedepo@gmail.com>
2020-07-20 08:52:41 +0100

    base url points to the live web api endpoint

--- NCELAP/NCELAP/wwwroot/js/util.js
+++ NCELAP/NCELAP/wwwroot/js/util.js
@@ -1,6 +1,6 @@
-﻿const baseUrl = 'https://localhost:44310/api/';
+﻿//const baseUrl = 'https://localhost:44310/api/';
 //const baseUrl = 'https://ncelap-demo-api.azurewebsites.net/api/';
-//const baseUrl = 'http://ncelaapi.azurewebsites.net/';
+const baseUrl = 'http://ncelaapi.azurewebsites.net/';
 
 
 function extractExtensionFromFileName(fileName) {
