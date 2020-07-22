68a1362ffe2b5675338a186a99ca0fc35f52ae5b
dflobt@gmail.com <dflobt@gmail.com>
2020-07-22 15:20:04 +0100

    point to demo

--- NCELAP/NCELAP/wwwroot/js/util.js
+++ NCELAP/NCELAP/wwwroot/js/util.js
@@ -1,9 +1,9 @@
 ﻿//const baseUrl = 'https://localhost:44310/api/';
 //const webBaseUrl = 'https://localhost:44374/';
-const baseUrl = 'https://ncelaapi.azurewebsites.net/api/';
-const webBaseUrl = 'https://ncelas.dpr.gov.ng/';
-//const baseUrl = 'https://ncelap-demo-api.azurewebsites.net/api/';
-//const webBaseUrl = 'https://ncelap-demo.azurewebsites.net/';
+//const baseUrl = 'https://ncelaapi.azurewebsites.net/api/';
+//const webBaseUrl = 'https://ncelas.dpr.gov.ng/';
+const baseUrl = 'https://ncelap-demo-api.azurewebsites.net/api/';
+const webBaseUrl = 'https://ncelap-demo.azurewebsites.net/';
  
 
 function extractExtensionFromFileName(fileName) {
